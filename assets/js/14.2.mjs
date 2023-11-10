2e0ad9b57508ccbfa2bfb579edb11a3f939457ea
Anastasiya McElnikova <sav20043+McElnikova@yandex.ru>
2023-11-10 21:15:19 +0300

    Дополнена панель навигации

--- assets/js/14.2.mjs
+++ assets/js/14.2.mjs
@@ -4,11 +4,18 @@ const logoSrc="./assets/img/logo.png";
 const navDataArr=[
     {
         caption: "Задача 1",
-        link: "./index1.html"
+        link: "./index1.html",
+        target: true
+    },
+    {
+        caption: "Тема",
+        link: "https://nwillis.notion.site/14-DOM-507f77a7a0b34c278c56dc6890b03173",
+        target: true
     },
     {
         caption: "Задача 2",
-        link: "./index.html"
+        link: "./index.html",
+        target: false
     }
 
 ];
@@ -42,8 +49,10 @@ function createNavElement(navData){
 //Создать узел из объекта navData
     let div = document.createElement('div');
     div.className = "nav-element";
+
+    let target=navData.target?'target="_blank"':"";
     div.innerHTML=`
-        <a href="${navData.link}">${navData.caption}</a>
+        <a href="${navData.link}" ${target}>${navData.caption}</a>
     `;
     nav.append(div);
 }
